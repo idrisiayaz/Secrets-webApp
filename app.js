03077356971d009392f7e5fa2f6ab1b1bffff5de
idrisiayaz <idrisiayaz@gmail.com>
2022-01-07 03:12:05 +0530

    srgsr

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(session({secret: "Our little secret." ,resave: false, saveUninitialized:
 app.use(passport.initialize());
 app.use(passport.session());
 
-mongoose.connect("mongodb+srv://idrisiayaz:ayaz1212@secrets.6azkq.mongodb.net/userDB", {useNewUrlParser: true});
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
 
 const userSchema = new mongoose.Schema({
     email: String,
@@ -157,11 +157,8 @@ app.post('/login', function(req, res) {
     });
 });
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-    port = 3000;
-}
 
-app.listen(port, function() {
-    console.log("Successfully started server!");
+
+app.listen(3000, function() {
+    console.log("Successfully started on port 3000");
 })
